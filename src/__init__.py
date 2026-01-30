2dcf3ded379f8de566a3bfdcd4b3b6193d44ae8b
ncail <n3var1as@gmail.com>
2026-01-29 18:29:10 -0700

    adding template for init for util functions (in progress)

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+from .utils import (
+    parse_arguments_mlp_input,
+    parse_arguments_train_model,
+    confirm_or_exit
+)
+
+
+
+__all__ = [
+    "parse_arguments_mlp_input",
+    "parse_arguments_train_model",
+    "confirm_or_exit"
+]
+
+
+
+
+
+
+
+
+
+
+
+
