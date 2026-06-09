59d661e8c8ba28e3848131d98d314b8e57abb39b
ncail <n3var1as@gmail.com>
2026-06-09 16:00:28 -0600

    exporting methods

--- src/rad7/__init__.py
+++ src/rad7/__init__.py
@@ -2,8 +2,15 @@
 Rad7 Analysis Package
 """
 
-from .io import read_r7raw, load_data_from_directory
-from .raw_parser import parse_raw_data
+from .io import (
+    read_r7raw, 
+    load_data_from_directory
+)
+
+from .raw_parser import (
+    parse_raw_data
+)
+
 from .processing import (
     process_run,
     process_runs,
@@ -12,6 +19,32 @@ from .processing import (
     stitch_gaps,
     place_time_cut
 )
-from .analysis import perform_fit, exponential_fit
-from .plotting import plot_concentration
-from .simulation import solve_diffusion_model
+
+from .analysis import (
+    perform_fit, 
+    exponential_fit
+)
+
+from .plotting import (
+    plot_concentration
+)
+
+from .simulation import (
+    solve_diffusion_model
+)
+
+__all__ = [
+    "read_r7raw",
+    "load_data_from_directory",
+    "parse_raw_data",
+    "process_run",
+    "process_runs",
+    "calibrate_data",
+    "rebin_data",
+    "stitch_gaps",
+    "place_time_cut",
+    "perform_fit",
+    "exponential_fit",
+    "plot_concentration",
+    "solve_diffusion_model"
+]
