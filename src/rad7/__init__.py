4524931ff438e84545ef7920a1d255b6fd2ea450
ncail <n3var1as@gmail.com>
2026-06-09 21:14:24 -0600

    exporting parsing utilities

--- src/rad7/__init__.py
+++ src/rad7/__init__.py
@@ -8,7 +8,10 @@ from .io import (
 )
 
 from .raw_parser import (
-    parse_raw_data
+    parse_raw_data,
+    expand_byte_column,
+    parse_flags_byte,
+    parse_units_byte
 )
 
 from .processing import (
@@ -37,6 +40,9 @@ __all__ = [
     "read_r7raw",
     "load_data_from_directory",
     "parse_raw_data",
+    "expand_byte_column",
+    "parse_flags_byte",
+    "parse_units_byte",
     "process_run",
     "process_runs",
     "calibrate_data",
